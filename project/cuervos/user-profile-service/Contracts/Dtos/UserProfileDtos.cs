7301f3ea6905c41fe278ea211f407fd9b1a02212
Javier Esteban Martínez Girón <96158215+javiermartinezgi01@users.noreply.github.com>
2025-12-09 07:49:48 -0500

    Delete project/cuervos directory

--- project/cuervos/user-profile-service/Contracts/Dtos/UserProfileDtos.cs
+++
@@ -1,5 +0,0 @@
-namespace UserProfileService.Contracts.Dtos;
-
-public record CreateUserProfileRequest(string Name, string Email, string? Description, DateTime? Birthdate);
-public record UpdateUserProfileRequest(string Name, string Email, string? Description, DateTime? Birthdate);
-public record UserProfileDto(int Id, string Name, string Email, string? Description, DateTime? Birthdate);
